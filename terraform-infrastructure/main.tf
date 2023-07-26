917d000a3b4002822038b97775f6e73a0c5650aa
Fred B <fredbitenyo@gmail.com>
2023-07-26 15:20:54 +0200

    correcting variables

--- terraform-infrastructure/main.tf
+++ terraform-infrastructure/main.tf
@@ -15,8 +15,6 @@ module "vpc" {
   public_subnet_az2_cidr       = var.public_subnet_az2_cidr
   private_app_subnet_az1_cidr  = var.private_app_subnet_az1_cidr
   private_app_subnet_az2_cidr  = var.private_app_subnet_az2_cidr
-  # private_data_subnet_az1_cidr = var.private_data_subnet_az1_cidr
-  # private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
 
 }
 
@@ -31,9 +29,6 @@ module "natgateway" {
   vpc_id                     = module.vpc.vpc_id
   private_app_subnet_az1_id  = module.vpc.private_app_subnet_az1_id
   private_app_subnet_az2_id  = module.vpc.private_app_subnet_az2_id
-  # private_data_subnet_az1_id = module.vpc.private_data_subnet_az1_id
-  # private_data_subnet_az2_id = module.vpc.private_data_subnet_az2_id
-
 }
 
 # create security group module
@@ -45,22 +40,6 @@ module "security-group" {
   ssh_ip       = var.ssh_ip
 
 }
-
-#  create rds module
-# module "rds" {
-#   source                    = "../terraform-modules/rds"
-#   project_name              = local.project_name
-#   environment               = local.environment
-#   private_app_subnet_az1_id = module.vpc.private_app_subnet_az1_id
-#   private_app_subnet_az2_id = module.vpc.private_app_subnet_az2_id
-#   db_snapshot_identifier    = var.db_snapshot_identifier
-#   db_instance_class         = var.db_instance_class
-#   availability_zone_1       = module.vpc.availability_zone_1
-#   db_instance_identifier    = var.db_instance_identifier
-#   multi_az_deployment       = var.multi_az_deployment
-#   database_sg_id            = module.security-group.database_sg_id
-
-# }
 
 #  create acm module
 module "acm" {
--- terraform-modules/natgateway/variables.tf
+++ terraform-modules/natgateway/variables.tf
@@ -7,5 +7,4 @@ variable "internet_gateway" {}
 variable "vpc_id" {}
 variable "private_app_subnet_az1_id" {}
 variable "private_app_subnet_az2_id" {}
-variable "private_data_subnet_az1_id" {}
-variable "private_data_subnet_az2_id" {}
+
